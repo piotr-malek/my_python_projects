03d07d5c8cf8b2ea633e141aeaa23144fa91bbb9
Piotr Malek <160703394+piotr-malek@users.noreply.github.com>
2024-08-30 14:21:02 +0200

    Delete trello directory

--- trello/errors.py
+++
@@ -1,7 +0,0 @@
-class ConfigError(Exception):
-    """Raised when there is an error with the configuration."""
-    pass
-
-class FetchError(Exception):
-    """Raised when there is an error fetching data from the Trello API."""
-    pass
